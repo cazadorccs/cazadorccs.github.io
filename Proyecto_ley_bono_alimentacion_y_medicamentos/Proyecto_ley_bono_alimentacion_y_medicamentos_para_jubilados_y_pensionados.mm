c05714968ac4b5603ed002576f16a2de97010994
sahib franco <cazadorccs@gmail.com>
2016-02-12 06:49:34 -0430

    Proyecto_ley_bono_alimentacion_y_medicamentos
    Nuevo proyecto Proyecto_ley_bono_alimentacion_y_medicamentos

---
+++ Proyecto_ley_bono_alimentacion_y_medicamentos/Proyecto_ley_bono_alimentacion_y_medicamentos_para_jubilados_y_pensionados.mm
@@ -0,0 +1,329 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#00b439" CREATED="1455274651241" ID="ID_700908575" MODIFIED="1455275529648">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PROYECTO DE LEY DE BONO DE ALIMENTACION Y MEDICAMENTOS PARA JUBILADOS Y PENSIONADOS
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#990000" CREATED="1452957134509" ID="ID_1565143888" MODIFIED="1455275529657" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Diagrama realizado
+    </p>
+    <p>
+      por el Cap. Sahib A. Franco
+    </p>
+  </body>
+</html></richcontent>
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1455274737490" FOLDED="true" ID="ID_1788312531" MODIFIED="1455275529658" POSITION="right" TEXT="Exposici&#xf3;n de Motivos">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455274744499" ID="ID_926944953" MODIFIED="1455275525607">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      En virtud de la responsabilidad del Estado de garantizar la seguridad social y atenci&#243;n integral a las personas mayores, consagrada en el art&#237;culo 80 de la Constituci&#243;n Nacional que establece: &#8220;El Estado garantizar&#225; a los ancianos y ancianas el pleno ejercicio de sus derechos y garant&#237;as. El Estado, con la participaci&#243;n solidaria de las familias y la sociedad, est&#225; obligado a respetar su dignidad humana, su autonom&#237;a y les garantizar&#225; atenci&#243;n integral y los beneficios de la seguridad social que eleven y aseguren su calidad de vida&#8221;....
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Es por esto, que el Estado venezolano en aras de cumplir con las obligaciones establecidas en la constituci&#243;n y en calidad de m&#225;ximo garante de los derechos humanos y de los derechos de los adultos mayores. Debe garantizar una adecuada alimentaci&#243;n y el acceso a las medicinas mediante programas sociales que est&#233;n contenidos en el ordenamiento jur&#237;dico venezolano.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Ese es el prop&#243;sito principal de este proyecto de ley que se somete a consideraci&#243;n de la Asamblea Nacional; ampliar el beneficio legal de alimentaci&#243;n que disfrutan los trabajadores a aquellas personas que dedicaron su vida productiva al trabajo y ahora se encuentran jubiladas o pensionadas. Este beneficio, que ha tenido gran &#233;xito y se ha convertido en uno de los beneficios m&#225;s apreciados por los trabajadores, es necesario que sea parte de los programas que ofrece el Estado a los adultos mayores, ofreci&#233;ndoles no solo un beneficio de alimentaci&#243;n sino tambi&#233;n la posibilidad de usar &#233;ste para la adquisici&#243;n de medicinas.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      La extensi&#243;n del beneficio de alimentaci&#243;n al jubilado o pensionado no es una petici&#243;n de unos pocos, sino un clamor popular. Es el reconocimiento a los ciudadanos que dedicaron su vida a ser &#243;rgano productivo de nuestro pa&#237;s. Es por ello, que no solo los interesados lo han solicitado ; sino todos los componentes de la sociedad han coincidido que se trata de un acto de reivindicaci&#243;n, de justicia social y una manifestaci&#243;n l&#243;gica y esperada del principio constitucional de progresividad que regula esta materia.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      El adulto mayor dedica fundamentalmente la pensi&#243;n o jubilaci&#243;n para su alimentaci&#243;n y salud. En el a&#241;o 2014 la inflaci&#243;n en Venezuela fue de 68,54% y para el mes de septiembre de 2015 la Canasta Alimentaria se encontraba en el orden de 38.873,12 bol&#237;vares seg&#250;n cifras INE y el monto de la pensi&#243;n era de 9.648,18 bol&#237;vares ( equivalente a un salario m&#237;nimo), por lo cual el deterioro del poder adquisitivo de su pensi&#243;n es dram&#225;tico. Esta circunstancia est&#225; obligando a nuestros pensionados y jubilados a decidir entre comer o cubrir un rubro absolutamente necesario como son los medicamentos, teniendo en cuenta que la mayor&#237;a de los Adultos Mayores son personas propensas a requerir de tratamientos m&#233;dicos permanentes que sobrepasan los ingresos de nuestros jubilados y pensionados.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Por eso la Asamblea Nacional, garante de los derechos d el pueblo y en el ejercicio leg&#237;timo de su poder de legislar en materias de competencia nacional, siente la necesidad y la urgencia de crear la Ley de Bono de Alimentaci&#243;n y de Bono de Medicamentos para Jubilados y Pensionados del sector p&#250;blico y del sector privado.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1455274793702" ID="ID_1058297519" MODIFIED="1455275529660" POSITION="right" TEXT="T&#xcd;TULO I">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455274813167" FOLDED="true" ID="ID_1306509222" MODIFIED="1455275525610" TEXT="Objeto de la Ley">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455274821355" FOLDED="true" ID="ID_1130739217" MODIFIED="1455275525611" TEXT="Objeto">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455274918407" ID="ID_1910225770" MODIFIED="1455275525612">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 1&#176;.</b>&#160;Esta Ley procura facilitar la buena alimentaci&#243;n y la adquisici&#243;n de medicinas por parte de los pensionados y jubilados del sector p&#250;blico y privado. La designaci&#243;n de personas en masculino tiene en las disposiciones de esta Ley un sentido gen&#233;rico, referido siempre por igual a hombres y mujeres.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1455274945551" ID="ID_1127155579" MODIFIED="1455275529666" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>T&#205;TULO II</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1455274975514" FOLDED="true" ID="ID_1269906690" MODIFIED="1455275525618" TEXT="De la protecci&#xf3;n social de los Jubilados y Pensionados Beneficiarios">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455274990015" FOLDED="true" ID="ID_40797937" MODIFIED="1455275525619">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 2&#176;.</b>&#160;A partir de la fecha de entrada en vigencia de esta Ley, el beneficio de alimentaci&#243;n se extender&#225; a los pensionados y jubilados del sector p&#250;blico y del sector privado. En el otorgamiento de este beneficio se tendr&#225;n en cuenta las reglas siguientes:
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1455275028052" ID="ID_62153433" MODIFIED="1455275525620">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      1. Los beneficiarios de una sola pensi&#243;n o jubilaci&#243;n otorgada por una entidad de trabajo del sector privado, por un ente del sector p&#250;blico o por el Instituto Venezolano de los Seguros Sociales, tendr&#225;n derecho a percibir el beneficio previsto en esta Ley por cuenta del pagador de la pensi&#243;n o jubilaci&#243;n, seg&#250;n sea el caso.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1455275028064" ID="ID_932522241" MODIFIED="1455275525621">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      2. Los beneficiarios de una pensi&#243;n o jubilaci&#243;n otorgada por una entidad de trabajo&#160;&#160;del&#160;&#160;sector&#160;&#160;privado,&#160;&#160;que&#160;&#160;simult&#225;neamente&#160; perciban&#160;&#160;una&#160;&#160;pensi&#243;n&#160;&#160;o jubilaci&#243;n otorgada por el Instituto Venezolano de los Seguros Sociales, tendr&#225;n derecho a percibir el beneficio previsto en esta Ley otorgado &#250;nicamente por el Instituto Venezolano de los Seguros Sociales.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1455275028069" ID="ID_512643169" MODIFIED="1455275525622">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3. Los beneficiarios de una pensi&#243;n o jubilaci&#243;n otorgada por una entidad de trabajo&#160;&#160;del&#160;&#160;sector&#160;&#160;privado,&#160;&#160;que&#160;&#160;simult&#225;neamente&#160; perciban&#160;&#160;una&#160;&#160;pensi&#243;n&#160;&#160;o jubilaci&#243;n otorgada por una empresa o ente del sector p&#250;blico, tendr&#225;n derecho a percibir el beneficio previsto en esta Ley otorgado &#250;nicamente por el ente p&#250;blico.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1455275028073" ID="ID_818528999" MODIFIED="1455275525624">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      4. Los beneficiarios de una pensi&#243;n o jubilaci&#243;n otorgada por una empresa o ente del sector p&#250;blico, que al mismo tiempo perciban una pensi&#243;n o jubilaci&#243;n otorgada por el Instituto Venezolano de los Seguros Sociales, tendr&#225;n derecho a percibir el beneficio previsto en esta Ley otorgado &#250;nicamente por el Instituto Venezolano de los Seguros Sociales.En todos los casos, quienes sean beneficiarios de dos o m&#225;s pensiones o jubilaciones recibir&#225;n el beneficio de parte de un solo ente, evitando as&#237; la duplicidad del beneficio.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1455275137712" FOLDED="true" ID="ID_1710421583" MODIFIED="1455275525628">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Monto del beneficio</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1455275156317" ID="ID_19816817" MODIFIED="1455275525629">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 3&#176;.</b>&#160;El valor del beneficio que de acuerdo a lo previsto en esta Ley se otorgue a los pensionados y jubilados ser&#225; equivalente a sesenta y siete unidad tributaria (67 U.T.) mensual.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1455275189115" FOLDED="true" ID="ID_715956745" MODIFIED="1455275525630" TEXT="Modalidades y plazo de entrega">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275212647" ID="ID_1220101434" MODIFIED="1455275525631">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 4&#176;.</b>&#160;La entrega del beneficio a los pensionados y jubilados se har&#225; mediante cupones, tickets, tarjetas electr&#243;nicas, o en dinero en efectivo o su equivalente, a elecci&#243;n del ente pagador. El beneficio se har&#225; efectivo en la misma oportunidad en la que se pague la respectiva pensi&#243;n o jubilaci&#243;n.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1455275248974" FOLDED="true" ID="ID_1029046437" MODIFIED="1455275525633" TEXT="Posibilidad de utilizar el beneficio tambi&#xe9;n para la adquisici&#xf3;n de medicinas">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275270020" ID="ID_1982243324" MODIFIED="1455275525634">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 5&#176;.</b>&#160;En los casos en que el beneficio se materialice a trav&#233;s de la entrega de cupones, tickets o tarjetas electr&#243;nicas, &#233;stas podr&#225;n ser utilizadas no solo para la compra de alimentos y comidas sino tambi&#233;n para la compra de medicinas en droguer&#237;as, farmacias y establecimientos similares.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1455275308068" ID="ID_1714602443" MODIFIED="1455275529674" POSITION="right" TEXT="T&#xcd;TULO III">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275332589" FOLDED="true" ID="ID_1356334310" MODIFIED="1455275525636" TEXT="Bono de medicamentos para los jubilados y pensionados con tratamientos m&#xe9;dicos permanentes">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275354516" ID="ID_1786556031" MODIFIED="1455275525638">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Organismo Ejecutor</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1455275391363" ID="ID_181553280" MODIFIED="1455275525638">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 6&#176;.</b>&#160;El Instituto Venezolano de los Seguros Sociales ser&#225; el ente del Ejecutivo Nacional encargado de la ejecuci&#243;n de este beneficio.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1455275426891" ID="ID_991561051" MODIFIED="1455275525639" TEXT="De la bonificaci&#xf3;n">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275451637" ID="ID_1730776221" MODIFIED="1455275525641">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>Art&#237;culo 7.</b>&#160;A los efectos del cumplimiento de esta Ley se otorgara esta bonificaci&#243;n de medicamentos, sin ning&#250;n tipo de discriminaci&#243;n a todas y todos los jubilados y pensionados, que est&#233;n sometidos a tratamientos m&#233;dicos permanentes y para tal efecto el IVSS, dictara el Reglamento respectivo que determinara los tratamientos m&#233;dicos permanentes que ser&#225;n cubiertos con este beneficio y los requisitos para su obtenci&#243;n, As&#237; como, tambi&#233;n aquellos que por su dif&#237;cil acceso al p&#250;blico y elevado costo, tendr&#225;n que seguir siendo suministrados por el estado venezolano.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1455275480434" ID="ID_1465736440" MODIFIED="1455275529678" POSITION="right" TEXT="T&#xcd;TULO V">
+<edge COLOR="#990000" WIDTH="1"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275490827" FOLDED="true" ID="ID_779690083" MODIFIED="1455275525643" TEXT="Disposiciones Finales">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275498001" ID="ID_1254162740" MODIFIED="1455275525644" TEXT="UNICA:">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node COLOR="#111111" CREATED="1455275506967" ID="ID_951361218" MODIFIED="1455275525645" TEXT="Esta Ley entrar&#xe1; en vigencia a partir de su publicaci&#xf3;n en la Gaceta Oficial de la Rep&#xfa;blica Bolivariana de Venezuela.Dado en Caracas, a los _______ d&#xed;as del mes de ______ de dos mil diecis&#xe9;is (2016). A&#xf1;os __&#xb0; de la Independencia y  ___&#xb0; de la Federaci&#xf3;n.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
