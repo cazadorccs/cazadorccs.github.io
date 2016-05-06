d5d2132243552815c96df6861fce1b566e84a73d
sahib franco <cazadorccs@gmail.com>
2016-05-06 10:42:43 -0400

    Juego_March_Empire
    Asientos de poder

---
+++ Asientos_de_Poder/Asientos del Poder.mm
@@ -0,0 +1,150 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1462544736977" ID="ID_546160024" MODIFIED="1462545283399" TEXT="Asientos del Poder">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<node COLOR="#00b439" CREATED="1462544763332" ID="ID_1182251422" MODIFIED="1462545283401" POSITION="right" TEXT="Concepto">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1462544771673" ID="ID_484675364" MODIFIED="1462545283410" TEXT="Los cinco Asientos del Poder rodean el Trono del Poder, que descansa en el centro de un reino. Las alianzas que los controlen podr&#xe1;n influir en el reino mediante la propuesta de pol&#xed;ticas mundiales. Adem&#xe1;s, cada asiento proporcionar&#xe1; una bonificaci&#xf3;n &#xfa;nica a la alianza que lo controle.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1462544900470" FOLDED="true" ID="ID_1588991200" MODIFIED="1462545283415" POSITION="right" TEXT="Reglas de los Asientos del Poder">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1462544913898" ID="ID_10714040" MODIFIED="1462545283416" TEXT="Solo las alianzas pueden capturar Asientos del Poder mediante concentraciones.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913900" ID="ID_961227695" MODIFIED="1462545283418" TEXT="Inicialmente, estar&#xe1; protegido por defensores no jugadores. Si la concentraci&#xf3;n de una alianza gana una batalla contra ellos, comenzar&#xe1; a correr un contador de disputa.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913904" ID="ID_1539960987" MODIFIED="1462545283419" TEXT="Las alianzas pueden iniciar una &#xfa;nica concentraci&#xf3;n contra cualquiera de los Asientos del Poder en cualquier momento.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913908" ID="ID_1537241725" MODIFIED="1462545283420" TEXT="Las alianzas pueden controlar solo un Asiento del Poder a la vez.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913909" ID="ID_83885538" MODIFIED="1462545283421" TEXT="Cuanto m&#xe1;s tiempo conserves el Asiento del Poder, mejores bonificaciones de fortificaci&#xf3;n recibir&#xe1;s.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913910" ID="ID_1635983940" MODIFIED="1462545283422" TEXT="Cuando el &quot;periodo de disputa del Asiento del Poder&quot; global finalice para todos los asientos, todas las alianzas que controlen los asientos pasar&#xe1;n a controlarlos. Si un Asiento del Poder no tiene due&#xf1;o, se escoger&#xe1; una pol&#xed;tica mundial aleatoria.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913916" ID="ID_497767300" MODIFIED="1462545283423" TEXT="Los asientos est&#xe1;n abiertos a disputa durante un periodo de 24 horas. A continuaci&#xf3;n, cada asiento propone una pol&#xed;tica durante las siguientes 24 horas. En un tercer periodo de 24 horas, se lleva a cabo la votaci&#xf3;n de las pol&#xed;ticas propuestas por los cinco asientos. La pol&#xed;tica ganadora es la vigente durante los siguientes cinco d&#xed;as.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462544913924" ID="ID_1129398254" MODIFIED="1462545283425" TEXT="Al final del periodo de disputa de los asientos, todos los ej&#xe9;rcitos ocupantes regresan a sus respectivos due&#xf1;os. Al inicio del siguiente periodo de disputa de los asientos, son liberados y pueden volver a conquistarse.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1462544789698" ID="ID_552690066" MODIFIED="1462545283426" POSITION="right" TEXT="Tipos de Asientos del Poder">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1462544832303" ID="ID_833664111" MODIFIED="1462545283427" TEXT="Asiento de la Guerra ">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1462544856389" ID="ID_1732817851" MODIFIED="1462545283429" TEXT="+20% de ataque y salud para todas las unidades">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1462545124273" ID="ID_1711094" MODIFIED="1462545283436" TEXT="Concentraciones b&#xe9;licas  Tama&#xf1;o de concentraci&#xf3;n +10%, pero el tiempo m&#xed;nimo de inicio de concentraci&#xf3;n es de 60 minutos.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545124278" ID="ID_32184151" MODIFIED="1462545283446" TEXT="Partidas de merodeadores  El tiempo de marcha se reduce un 50% al atacar, pero la carga total del ej&#xe9;rcito se reduce un 75%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545124282" ID="ID_585387564" MODIFIED="1462545283456" TEXT="Hospitales de campa&#xf1;a  Incluso tras batallas fuera de tu ciudad, las unidades siempre van al templo, pero su coste de curaci&#xf3;n se multiplica por 3.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545124285" ID="ID_1447964082" MODIFIED="1462545283460" TEXT="Trampas mortales  Efectividad de defensa +200%, y el coste de reparaci&#xf3;n de trampas se reduce un 50%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1462544832305" ID="ID_862544535" MODIFIED="1462545283463" TEXT="Asiento del Orden">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1462544866151" ID="ID_1390917409" MODIFIED="1462545283465" TEXT="+200% Ingresos de recursos">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1462545168183" ID="ID_27163229" MODIFIED="1462545283466" TEXT="Foco en producci&#xf3;n  Ingresos de recursos +250%, pero los recursos protegidos se reducen un 50%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545168184" ID="ID_321419288" MODIFIED="1462545283472" TEXT="Ej&#xe9;rcito profesional  El gasto de las unidades ahora se paga con plata en vez de con comida y se reduce un 75%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545168187" ID="ID_757515630" MODIFIED="1462545283476" TEXT="Reclutamiento  Velocidad de entrenamiento +125%, pero el coste de las unidades se multiplica por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545168188" ID="ID_1055277661" MODIFIED="1462545283477" TEXT="Foco en investigaci&#xf3;n  Velocidad de investigaci&#xf3;n +50%, pero el coste de investigaci&#xf3;n se multiplica por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1462544832307" ID="ID_1533295347" MODIFIED="1462545283478" TEXT="Asiento del Dominio">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1462544873933" ID="ID_1184437168" MODIFIED="1462545283480" TEXT="+25% de velocidad de marcha">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1462545199309" ID="ID_900774503" MODIFIED="1462545283486" TEXT="Explotaci&#xf3;n de recursos  Velocidad de recolecci&#xf3;n+35%, pero la carga de unidades necesaria se multiplica por 3.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545199314" ID="ID_445058154" MODIFIED="1462545283497" TEXT="Patrulla del reino  Los ataques contra campamentos JcE tienen una bonificaci&#xf3;n de +75% a la salud de unidad.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545199319" ID="ID_805294527" MODIFIED="1462545283500" TEXT="Preparaciones estacionales  Las bonificaciones estacionales se multiplican por 5.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545199320" ID="ID_230813019" MODIFIED="1462545283501" TEXT="Prospecciones de oro  Aparecer&#xe1;n minas de oro ocasionalmente en el mapa del mundo.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1462544832309" ID="ID_993899852" MODIFIED="1462545283505" TEXT="Asiento del Pacto">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1462544879300" ID="ID_1500133589" MODIFIED="1462545283506" TEXT="+30 Puntos de influencia">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1462545240695" ID="ID_1092560692" MODIFIED="1462545283511" TEXT="Libre comercio  Los puestos comerciales generan 2 ofertas comerciales adicionales, pero la plata reunida por puesto comercial se reduce un 50%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545240701" ID="ID_1692084879" MODIFIED="1462545283518" TEXT="Inversiones regionales  Los efectos adicionales de establos, murallas regionales y lugares sagrados se multiplican por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545240705" ID="ID_814265021" MODIFIED="1462545283525" TEXT="Log&#xed;stica de alianza  Los jugadores en una alianza consiguen 1 marcha adicional, pero la velocidad de marcha aumenta un 30%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545240708" ID="ID_1173486536" MODIFIED="1462545283528" TEXT="Capitales santuario  Capacidad de las capitales +25%, pero la duraci&#xf3;n de la disputa se reduce un 50%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1462544832310" ID="ID_1120424205" MODIFIED="1462545283529" TEXT="Asiento de los H&#xe9;roes">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1462544887904" ID="ID_41342639" MODIFIED="1462545283530" TEXT="+20% Vel. entrenam. de unidad de facci&#xf3;n">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1462545260473" ID="ID_225831388" MODIFIED="1462545283535" TEXT="Guardianes valerosos  El escudo tambi&#xe9;n protege las marchas de recolecci&#xf3;n, pero el tiempo total de recolecci&#xf3;n aumenta un 50%.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545260477" ID="ID_1855622521" MODIFIED="1462545283541" TEXT="Campeones aventureros  Premios de aventuras +300%, pero el tiempo y coste de energ&#xed;a de las aventuras se multiplican por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545260481" ID="ID_857154420" MODIFIED="1462545283548" TEXT="Entrenamiento legendario  Velocidad de entrenamiento de unidades de facci&#xf3;n +50%, pero el coste de entrenamiento se multiplica por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1462545260484" ID="ID_1986775932" MODIFIED="1462545283551" TEXT="H&#xe9;roes de distrito  Las bonificaciones de distrito se multiplican por 2.">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1462544959366" ID="ID_1743249640" MODIFIED="1462545283552" POSITION="right" TEXT="Votaci&#xf3;n de los Asientos del Poder">
+<edge COLOR="#00b439" WIDTH="2"/>
+<node COLOR="#990000" CREATED="1462545104070" ID="ID_731055041" MODIFIED="1462545283553" TEXT="Cada asiento tiene sus propias pol&#xed;ticas mundiales que puede proponer para su votaci&#xf3;n global.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462545104072" ID="ID_830326692" MODIFIED="1462545283559" TEXT="El l&#xed;der de una alianza que controle un Asiento del Poder podr&#xe1; proponer una pol&#xed;tica mundial entre aquellas que est&#xe9;n disponibles para este durante el periodo de propuesta.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462545104076" ID="ID_1222442626" MODIFIED="1462545283565" TEXT="Al comienzo del periodo de votaci&#xf3;n, se recogen las cinco propuestas y cada jugador del reino podr&#xe1; votar por una de ellas.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462545104080" ID="ID_1902994208" MODIFIED="1462545283571" TEXT="Al final del periodo de votaci&#xf3;n, se anuncian los resultados de la votaci&#xf3;n y la pol&#xed;tica mundial que cuente con el mayor n&#xfa;mero de votos ser&#xe1; la activa durante los siguientes cinco d&#xed;as.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1462545104087" ID="ID_1788180834" MODIFIED="1462545283577" TEXT="El asiento que propuso la pol&#xed;tica ganadora consigue una bonificaci&#xf3;n doble durante los siguientes cuatro d&#xed;as.">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+</map>
